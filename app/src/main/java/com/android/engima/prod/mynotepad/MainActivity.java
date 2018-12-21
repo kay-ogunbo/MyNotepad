3402d217ca05be78454905744f8ad05e5c712ac6
Ogunbowale <O.Ogunbowal@shell.com>
2018-12-21 19:08:29 +0100

    Initial commit

---
+++ app/src/main/java/com/android/engima/prod/mynotepad/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.engima.prod.mynotepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
